ef2c1aae701e1b873df0a4608d6b1602f108324d
milad1367 <miladasghari2016@gmail.com>
2017-05-20 16:50:43 -0700

    responsive search done

--- js/main.js
+++ js/main.js
@@ -19,10 +19,10 @@ $(document).click(function() {
     nav.width(0);
    }
 });
+
 $('.nav_open').click(function(){
    document.getElementById("nav").style.width = "150px";
 })
-
 
 function closeNav() {
     document.getElementById("nav").style.width = "0";
