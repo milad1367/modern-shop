0ec598c800f06752584c4c69091aa7f2b8a44ce4
milad <miladasghari2016@gmail.com>
2017-06-01 21:34:01 +0430

    resolve slick remove infinity

--- js/main.js
+++ js/main.js
@@ -55,6 +55,58 @@ $('.off_products').slick({
   ]
 });
 
+$('.new_products').slick({
+  dots: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 5,
+  slidesToScroll: 4,
+   arrows : false,
+  responsive: [
+    {
+      breakpoint: 1300,
+        settings: {
+        slidesToShow: 4,
+        slidesToScroll: 3,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 1100,
+        settings: {
+        slidesToShow: 4,
+        slidesToScroll: 3,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 2,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
 
 });
 
