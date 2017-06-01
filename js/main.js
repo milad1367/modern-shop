e2e006b4bea8e895476509afb90fa0c823206795
milad <miladasghari2016@gmail.com>
2017-06-02 00:26:21 +0430

    direction

--- js/main.js
+++ js/main.js
@@ -67,38 +67,40 @@ $('.new_products').slick({
       breakpoint: 1300,
         settings: {
         slidesToShow: 4,
-        slidesToScroll: 3,
+        slidesToScroll: 1,
         dots: true
       }
     },
     {
       breakpoint: 1100,
         settings: {
-        slidesToShow: 4,
-        slidesToScroll: 3,
-        dots: true
-      }
-    },
-    {
-      breakpoint: 1024,
-      settings: {
         slidesToShow: 3,
         slidesToScroll: 2,
         dots: true
       }
     },
     {
-      breakpoint: 600,
+      breakpoint: 1024,
       settings: {
         slidesToShow: 2,
-        slidesToScroll: 2
+        slidesToScroll: 3,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true
       }
     },
     {
       breakpoint: 480,
       settings: {
         slidesToShow: 1,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        dots: true
       }
     }
     // You can unslick at a given breakpoint now by adding:
